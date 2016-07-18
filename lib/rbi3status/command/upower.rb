d8c7700036dfbf8de84e3cee443f9997806db4fd
Dan Collis-Puro <dan@collispuro.net>
2016-07-17 20:39:36 -0400

    Correct for when battery is full

--- lib/rbi3status/command/upower.rb
+++ lib/rbi3status/command/upower.rb
@@ -1,12 +1,20 @@
 module Rbi3status
   module Command
     class Upower < Base
+      attr_reader :device
+
+      def initialize(options = {})
+        super(options)
+
+        @device = options[:device] || '/org/freedesktop/UPower/devices/battery_BAT0'
+      end
+
       def run
         get_upower_info
 
         info = {
           full_text: full_message,
-          short_text: time,
+          short_text: full_message,
           name: "time",
           urgent:  false,
           instance: "time",
@@ -20,7 +28,9 @@ module Rbi3status
       end
 
       def full_message
-        if charging?
+        if full?
+          %Q|⚡ CHR #{percent}|
+        elsif charging?
           %Q|⚡ CHR #{percent} #{time}|
         else
           %Q|⚡ BAT #{percent} #{time}|
@@ -28,19 +38,24 @@ module Rbi3status
       end
 
       def get_upower_info
-        @upower_info = `upower -i /org/freedesktop/UPower/devices/battery_BAT0`.split(/\n/)
+        @upower_info = `upower -i #{device}`.split(/\n/)
+      end
+
+      def full?
+        time.nil?
       end
 
       def percent
-        @upower_info.find{|f| f.match(/percentage/) }.split(':')[1].strip
+        @upower_info.find{ |f| f.match(/percentage/) }.split(':')[1].strip
       end
 
       def time
-        @upower_info.find{|f| f.match(/time/) }.split(':')[1].strip
+        time_attribute = @upower_info.find{ |f| f.match(/time/) }
+        time_attribute.split(':')[1].strip if time_attribute
       end
 
       def state
-        @upower_info.find{|f| f.match(/state/) }.split(':')[1].strip
+        @upower_info.find{ |f| f.match(/state/) }.split(':')[1].strip
       end
 
       def charging?
